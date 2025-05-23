52dc242cf8ed2a404a4d1593a62be554f17930a7
Csaba Molnar <docforloop@proton.me>
2025-05-23 22:51:16 +0200

    Add mobile main styles for location page

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,43 @@
 import './scss/style.scss';
+
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import customIconUrl from './assets/images/icon-location.svg'
+
+const rootStyles = getComputedStyle(document.documentElement);
+const mapCircle = rootStyles.getPropertyValue('--clr-map-circle').trim();
+const mapFill = rootStyles.getPropertyValue('--clr-map-fill').trim();
+
+const customIcon = L.icon({
+  iconUrl: customIconUrl,
+  iconSize: [66, 88],
+  iconAnchor: [33, 88],
+  popupAnchor: [0, -72],
+});
+
+const map = L.map('map').setView([41.482144, -71.310514], 16);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+L.marker([41.482144, -71.310514], {
+    icon: customIcon, 
+    alt: 'Modern Art Gallery, 99 King Street, Newport, RI 02840, USA' })
+  .addTo(map)
+  .bindPopup('<b>Modern Art Gallery</b><br>99 King Street<br>Newport<br>RI 02840<br>USA')
+  .openPopup();
+  
+L.circle([41.482144, -71.310514], {
+  radius: 300,
+  color: mapCircle,
+  fillColor: mapFill,
+  fillOpacity: 1,
+  interactive: false,
+}).addTo(map);
+
+map.zoomControl.remove();
+L.control.zoom({
+  position: 'bottomright'
+}).addTo(map);
